132df02f757f8656d5ad107b9875bd4a4c36dd9c
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-01 19:01:01 +0800

    add tcp

--- android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.java
@@ -56,6 +56,7 @@ import versioned.host.exp.exponent.modules.api.UtilModule;
 import versioned.host.exp.exponent.modules.api.WebBrowserModule;
 import versioned.host.exp.exponent.modules.api.av.AVModule;
 import versioned.host.exp.exponent.modules.api.av.video.VideoViewManager;
+import versioned.host.exp.exponent.modules.api.components.tcp.TcpSocketsModule;
 import versioned.host.exp.exponent.modules.api.standalone.branch.RNBranchModule;
 import versioned.host.exp.exponent.modules.api.components.LinearGradientManager;
 import versioned.host.exp.exponent.modules.api.components.barcodescanner.BarCodeScannerModule;
@@ -188,7 +189,8 @@ public class ExponentPackage implements ReactPackage {
     addViewManagersFromPackages(reactContext, viewManagers, Arrays.<ReactPackage>asList(
       new SvgPackage(),
       new MapsPackage(),
-      new LottiePackage()
+      new LottiePackage(),
+      new TcpSocketsModule()
     ));
 
     return viewManagers;
